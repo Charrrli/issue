c870dc8aa564abc38a835a363a8c152b09cfaf92
Charrrli <lzl596355806@qq.com>
2021-11-30 17:07:23 +0800

    change dic updata fix data

--- try_creepe/__init__.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import requests
-data = pd.read_csv("../data/raw_data.csv",nrows = 1)
-target = data.comments_url[0]
-json_data = requests.get(target)
-for i in json_data.json():
-    print(i)
-    print("\n")
